ad0b04705c88bafa454f5f702a31cc597637ea90
SweetBrioche <104081325+SweetBrioche@users.noreply.github.com>
2022-05-24 16:02:36 +0300

    Update week11code.js

--- week11code.js
+++ week11code.js
@@ -10,7 +10,7 @@ function init() {
 	 document.body.appendChild(renderer.domElement);
 
 	 var camera=new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 20000);
-	camera.position.z = 300;
+	camera.position.z = 150;
 
 	 scene.add(camera);
 
